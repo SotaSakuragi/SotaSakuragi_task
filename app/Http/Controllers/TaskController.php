d2ef6191095530c7372adc0fcc5665bd5458428c
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-10-30 15:12:23 +0900

    showとtaskのテンプレート完成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $tasks = Task::all();
+        // tasksディレクトリーの中のindexページを指定し、tasksの連想配列を代入
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    //showページへ移動
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.show', ['task' => $task]);
+    }
+    
 }
